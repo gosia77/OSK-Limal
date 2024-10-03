e35ef3ce0b199bbb009d35f1537029b968ee6d87
unknown <malgorzataserafinlatka@gmail.com>
2024-10-03 15:16:17 +0200

    test

--- slider.js
+++ slider.js
@@ -1,44 +1,34 @@
 const slides = [{
-  path: 'gallery/skoda.jpg',
-  alt: 'Ośrodek Szkolenia Kierowców LimaL',
-  srcset: 'gallery/skoda-small.png 400w, gallery/skoda-medium.png 800w, gallery/skoda-large.png 1200w',
-  sizes: '(max-width: 400px) 400px, (max-width: 800px) 800px, 1200px'
-},
-{
-  path: 'gallery/skoda2.jpg',
-  alt: 'Ośrodek Szkolenia Kierowców LimaL',
-  srcset: 'gallery/skoda2-small.png 400w, gallery/skoda2-medium.png 800w, gallery/skoda2-large.png 1200w',
-  sizes: '(max-width: 400px) 400px, (max-width: 800px) 800px, 1200px',
-},
-{
-  path: 'gallery/plac.jpg',
-  alt: 'Ośrodek Szkolenia Kierowców LimaL',
-  srcset: 'gallery/plac-small.jpg 400w, gallery/plac-medium.jpg 800w, gallery/plac-large.jpg 1200w',
-  sizes: '(max-width: 400px) 400px, (max-width: 800px) 800px, 1200px',
-},
-{
-  path: 'gallery/plac2.jpg',
-  alt: 'Ośrodek Szkolenia Kierowców LimaL',
-  srcset: 'gallery/plac2-small.jpg 400w, gallery/plac2-medium.jpg 800w, gallery/plac2-large.jpg 1200w',
-  sizes: '(max-width: 400px) 400px, (max-width: 800px) 800px, 1200px',
-},
-
+    path: 'gallery/skoda.jpg',
+    alt: 'Ośrodek Szkolenia Kierowców LimaL'
+  },
+  {
+    path: 'gallery/skoda2.jpg',
+    alt: 'Ośrodek Szkolenia Kierowców LimaL'
+  },
+  {
+    path: 'gallery/plac.jpg',
+    alt: 'Ośrodek Szkolenia Kierowców LimaL'
+  },
+  {
+    path: 'gallery/plac2.jpg',
+    alt: 'Ośrodek Szkolenia Kierowców LimaL'
+  },
+  
 ];
 
 const swiperWrapper = document.querySelector('.swiper-wrapper');
 
 for (const slide of slides) {
-  const slideNode = document.createElement('div');
-  slideNode.classList.add('swiper-slide')
+    const slideNode = document.createElement('div');
+    slideNode.classList.add('swiper-slide')
 
-  const imgNode = document.createElement('img')
-  imgNode.src = slide.path;
-  imgNode.alt = slide.alt;
-  imgNode.srcset = slide.srcset;
-  imgNode.sizes = slide.sizes;
+    const imgNode = document.createElement('img')
+    imgNode.src = slide.path;
+    imgNode.alt = slide.alt;
 
-  slideNode.appendChild(imgNode)
+    slideNode.appendChild(imgNode)
 
-  swiperWrapper.appendChild(slideNode);
+    swiperWrapper.appendChild(slideNode);
 }
 
