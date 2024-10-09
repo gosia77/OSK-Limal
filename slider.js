9802c1d85e92f320fcdcc00bdec4ae194dfe6559
Kamil Serafin <wa12rior@gmail.com>
2024-10-09 21:44:10 +0200

    feat: fix more styles and html

--- slider.js
+++ slider.js
@@ -1,56 +1,57 @@
-const slides = [{
-    srcset:`
-      gallery/skoda-320w.jpg,
-      gallery/skoda-480w.jpg,
-      gallery/skoda-640w.jpg`,
-    path: 'gallery/skoda.jpg',
-    alt: 'Ośrodek Szkolenia Kierowców LimaL'
+const slides = [
+  {
+    srcset1280: "gallery/skoda.jpg",
+    srcset768: "gallery/skoda-640w.jpg",
+    defaultPath: "gallery/skoda-480w.jpg",
+    alt: "Ośrodek Szkolenia Kierowców LimaL",
   },
   {
-    srcset:`
-      gallery/skoda2-320w.jpg,
-      gallery/skoda2-480w.jpg,
-      gallery/skoda2-640w.jpg`,
-    path: 'gallery/skoda2.jpg',
-    alt: 'Ośrodek Szkolenia Kierowców LimaL'
+    srcset1280: "gallery/skoda2.jpg",
+    srcset768: "gallery/skoda2-640w.jpg",
+    defaultPath: "gallery/skoda2-480w.jpg",
+    alt: "Ośrodek Szkolenia Kierowców LimaL",
   },
   {
-    srcset:`
-      gallery/plac-320w.jpg,
-      gallery/plac-480w.jpg,
-      gallery/plac-640w.jpg`,
-    path: 'gallery/plac.jpg',
-    alt: 'Ośrodek Szkolenia Kierowców LimaL'
+    srcset1280: "gallery/plac.jpg",
+    srcset768: "gallery/plac-640w.jpg",
+    defaultPath: "gallery/plac-480w.jpg",
+    alt: "Ośrodek Szkolenia Kierowców LimaL",
   },
   {
-    srcset:`
-      gallery/plac2-320w.jpg,
-      gallery/plac2-480w.jpg,
-      gallery/plac2-640w.jpg`,
-    path: 'gallery/plac2.jpg',
-    alt: 'Ośrodek Szkolenia Kierowców LimaL'
-  }, 
+    srcset1280: "gallery/plac2.jpg",
+    srcset768: "gallery/plac2-640w.jpg",
+    defaultPath: "gallery/plac2-480w.jpg",
+    alt: "Ośrodek Szkolenia Kierowców LimaL",
+  },
 ];
 
-const swiperWrapper = document.querySelector('.swiper-wrapper');
+const swiperWrapper = document.querySelector(".swiper-wrapper");
 
-for (const slide of slides) {
-    const slideNode = document.createElement('div');
-    slideNode.classList.add('swiper-slide');
+for (const { srcset1280, srcset768, defaultPath, alt } of slides) {
+  const slideNode = document.createElement("picture");
+  slideNode.classList.add("swiper-slide");
 
-    const imgNode = document.createElement('img');
-    imgNode.srcset = slide.srcset;
-    imgNode.src = slide.path;
-    imgNode.alt = slide.alt;
-    imgNode.sizes = "(max-width:320px) 280px, (max-width: 480px) 440px, 640px";
-    imgNode.loading = 'lazy';
+  const source1 = document.createElement("source");
+  const source2 = document.createElement("source");
 
-    imgNode.onerror = () => {
-      imgNode.src = 'gallery/default.jpg';
-    }
+  source1.media = "(min-width: 1280px)";
+  source1.srcset = srcset1280;
+  source2.media = "(min-width: 768px)";
+  source2.srcset = srcset768;
 
-    slideNode.appendChild(imgNode)
+  const imgNode = document.createElement("img");
+  imgNode.src = defaultPath;
+  imgNode.alt = alt;
+  imgNode.sizes = "(max-width:320px) 280px, (max-width: 480px) 440px, 640px";
+  imgNode.loading = "lazy";
 
-    swiperWrapper.appendChild(slideNode);
+  imgNode.onerror = () => {
+    imgNode.src = "gallery/default.jpg";
+  };
+
+  slideNode.appendChild(source1);
+  slideNode.appendChild(source2);
+  slideNode.appendChild(imgNode);
+
+  swiperWrapper.appendChild(slideNode);
 }
-
