3480603983b124b35d5b50a4a7e2b912d40ac92c
PCGosia <malgorzataserafinlatka@gmail.com>
2026-02-26 09:19:51 +0100

    Add changes

--- index.js
+++ index.js
@@ -18,14 +18,3 @@ scrollButton.addEventListener("click", () => {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 });
 
-// reCaptacha
-
-function onClick(e) {
-  e.preventDefault();
-  grecaptcha.enterprise.ready(async () => {
-    const token = await grecaptcha.enterprise.execute(
-      "6LfHaU4qAAAAAPEpVprtAxYbi-r_zXJJtGtyMkGh",
-      { action: "LOGIN" }
-    );
-  });
-}
